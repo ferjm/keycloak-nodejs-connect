ae35a599881fe912b3cec8503b90307f26a0eda0
Jan Jongboom <janjongboom@gmail.com>
2025-02-17 11:16:11 +0100

    NOT A CONTRIBUTION notice

--- example/index.js
+++ example/index.js
@@ -1,5 +1,8 @@
 /*
  * Copyright 2016 Red Hat Inc. All rights reserved.
+ * Modifications Copyright 2022 EdgeImpulse Inc.
+ *
+ * NOT A CONTRIBUTION
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
